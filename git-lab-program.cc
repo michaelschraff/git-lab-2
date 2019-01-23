6dee09520a4f4ec4afc09d75ac48f3f97d9df8c0
Michael Schraff <ms773317@ohio.edu>
2019-01-22 19:11:01 -0500

    Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Michael Schraff
+ *        Date: 1/22/19
+ * Description: Git Example
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+    return 0;
+}// main
